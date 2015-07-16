0bfdb81b900ce63244490f67d824c7115597ac0a
malonem3 <malonem3@udayton.edu>
2015-07-16 09:45:01 -0400

    Developed the Dominant Flow and Console Ouptut for the Data Analyzer tab in the GUI.

--- src/edu/salisbury/photonic/core_simulator/MainGUI.java
+++ src/edu/salisbury/photonic/core_simulator/MainGUI.java
@@ -18,14 +18,19 @@ import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.custom.StyledText;
 
 
 public class MainGUI {
+	//For latency calculation
 	public static int totalRequestingTime = 0;
 	public static int totalTasks = 0;
 	
+	//GUI Window
 	protected Shell shellRONoC;
 	protected Display display;
+	
+	//Simulation Variables (= default values)
 	protected ProgressBar progressBar;
 	protected TabFolder tabFolderMenu;
 	private String simulatorTopology = "Ring";
@@ -33,7 +38,12 @@ public class MainGUI {
 	private int simulatorTearDownTime = 1;
 	//private int simulatorNumOfSwitches = 0;
 	private Canvas canvasTopologyPreview;;
-	public static Label lblConsoleOutput;
+	private Label lblConsoleOutput;
+	
+	//Data Analyzer Variables (= default values)
+	private String fileName = "flow_barnes.log";
+	private int numOfSections = 1;
+	private StyledText styledTextConsoleOutput;
 
 
 	/**
@@ -102,8 +112,21 @@ public class MainGUI {
 		//_______________________________________________//
 		
 		//////////////DATA ANALYZER TAB////////////////////
+		
 		TabItem tbtmDataAnalyzer = new TabItem(tabFolderMenu, SWT.NONE);
 		tbtmDataAnalyzer.setText("Data Analyzer");
+		
+		Group grpDataAnalyzer = new Group(tabFolderMenu, SWT.NONE);
+		tbtmDataAnalyzer.setControl(grpDataAnalyzer);
+		
+		//Dominant Flow group... consists of options to run the dominant flow analyzer
+		createDominantFlowAnalyzer(grpDataAnalyzer);
+		
+		//Genetic algorithm group... options to modify and run genetic algorithm
+		createGeneticAlgorithmAnalyzer(grpDataAnalyzer);
+		
+		//Console Output group... prints out the data to this console
+		createConsoleOutputDA(grpDataAnalyzer);
 		
 		
 		///////////////////////////////////////////////////
@@ -280,7 +303,7 @@ public class MainGUI {
 		
 	}
 	
-	public static void printToConsole(String message) {
+	public void printToConsole(String message) {
 		lblConsoleOutput.setText(message);
 	}
 	
@@ -316,4 +339,159 @@ public class MainGUI {
 		btnSimulate.setText("Simulate");
 		//end simulator button
 	}
+	
+	private void createDominantFlowAnalyzer(Group grpDataAnalyzer) {
+		Group grpDominantFlow = new Group(grpDataAnalyzer, SWT.NONE);
+		grpDominantFlow.setText("Dominant Flow");
+		grpDominantFlow.setBounds(11, 9, 430, 273);
+		
+		ToolBar toolBarInputLog = new ToolBar(grpDominantFlow, SWT.BORDER | SWT.FLAT | SWT.WRAP | SWT.RIGHT | SWT.VERTICAL);
+		toolBarInputLog.setBounds(12, 41, 197, 191);
+		
+		ToolItem tltmBarnesLog = new ToolItem(toolBarInputLog, SWT.RADIO);
+		tltmBarnesLog.setSelection(true);
+		tltmBarnesLog.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				fileName = "flow_barnes.log";
+			}
+		});
+		tltmBarnesLog.setText("o Barnes Log");
+		
+		Label lblInputLog = new Label(grpDominantFlow, SWT.NONE);
+		lblInputLog.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
+		lblInputLog.setBounds(83, 20, 55, 15);
+		lblInputLog.setText("Input Log");
+		
+		Label lblSectionsOfLog = new Label(grpDominantFlow, SWT.NONE);
+		lblSectionsOfLog.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.BOLD));
+		lblSectionsOfLog.setBounds(277, 20, 85, 15);
+		lblSectionsOfLog.setText("Sections of Log");
+		
+		ToolBar toolBarSectionsOfLog = new ToolBar(grpDominantFlow, SWT.BORDER | SWT.FLAT | SWT.WRAP | SWT.RIGHT | SWT.VERTICAL);
+		toolBarSectionsOfLog.setBounds(221, 41, 197, 191);
+		
+		ToolItem tltmFullSection = new ToolItem(toolBarSectionsOfLog, SWT.RADIO);
+		tltmFullSection.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numOfSections = 1;
+			}
+		});
+		tltmFullSection.setSelection(true);
+		tltmFullSection.setWidth(36);
+		tltmFullSection.setText("o Full Section       ");
+		
+		ToolItem tltmHalfSections = new ToolItem(toolBarSectionsOfLog, SWT.RADIO);
+		tltmHalfSections.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numOfSections = 2;
+			}
+		});
+		tltmHalfSections.setText("o Half Sections    ");
+		
+		ToolItem tltmFourthSections = new ToolItem(toolBarSectionsOfLog, SWT.RADIO);
+		tltmFourthSections.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				numOfSections = 4;
+			}
+		});
+		tltmFourthSections.setText("o Fourth Sections");
+		
+		Button btnDominantFlow = new Button(grpDominantFlow, SWT.NONE);
+		btnDominantFlow.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				analyzeDominantFlow(); //make this a method call once made the class below
+			}
+		});
+		btnDominantFlow.setBounds(345, 238, 75, 25);
+		btnDominantFlow.setText("Analyze!");
+		//end dominant flow group
+		
+	}
+	
+	private void analyzeDominantFlow() {
+		//TODO just make this function into a class!
+		CoreLog coreLog = new CoreLog();
+		coreLog = LogReader.readLogIgnoreRepeaters(fileName);
+		NonDirectionalPairAnalyzer dominantFlow = new NonDirectionalPairAnalyzer();
+		String message;
+		switch(numOfSections) {
+			case 1:
+				message = "The dominant flow enumration across the full log: \n";
+				printToConsoleDA(message + dominantFlow.analyze(coreLog));
+				break;
+			case 2:
+				CoreLog firstHalf = new CoreLog();
+				firstHalf = coreLog.subLog(0, coreLog.logSize()/2 - 1);
+				message = "The first half of the barnes log dominant flows: \n";
+				message += dominantFlow.analyze(firstHalf);
+				
+				CoreLog secondHalf = new CoreLog();
+				secondHalf = coreLog.subLog(coreLog.logSize()/2, coreLog.logSize() - 1);
+				message += "\n\nThe second half of the barnes log dominant flows: \n";
+				printToConsoleDA(message + dominantFlow.analyze(secondHalf));
+				break;
+			case 4:
+				CoreLog firstForth = new CoreLog();
+				firstForth = coreLog.subLog(0, coreLog.logSize()/4 - 1);
+				message = "The first forth of the barnes log dominant flows: \n";
+				message += dominantFlow.analyze(firstForth);
+				
+				CoreLog secondForth = new CoreLog();
+				secondForth = coreLog.subLog(coreLog.logSize()/4, coreLog.logSize()/2 - 1);
+				message += "\n\nThe second forth of the barnes log dominant flows: \n";
+				message += dominantFlow.analyze(secondForth);
+				
+				CoreLog thirdForth = new CoreLog();
+				thirdForth = coreLog.subLog(coreLog.logSize()/2, coreLog.logSize()*3/4 - 1);
+				message += "\n\nThe third forth of the barnes log dominant flows: \n";
+				message += dominantFlow.analyze(thirdForth);
+				
+				CoreLog lastForth = new CoreLog();
+				lastForth = coreLog.subLog(coreLog.logSize()*3/4, coreLog.logSize() - 1);
+				message += "\n\nThe last forth of the barnes log dominant flows: \n";
+				printToConsoleDA(message + dominantFlow.analyze(lastForth));
+				break;
+			default:
+				//throw new ...
+		}
+	}
+	
+	public void printToConsoleDA(String message) {
+		styledTextConsoleOutput.setText(message);
+	}
+	
+	private void createGeneticAlgorithmAnalyzer(Group grpDataAnalyzer) {
+		Group grpGeneticAlgorithm = new Group(grpDataAnalyzer, SWT.NONE);
+		grpGeneticAlgorithm.setText("Genetic Algorithm");
+		grpGeneticAlgorithm.setBounds(11, 288, 430, 270);
+		
+		Button btnGeneticAlgorithm = new Button(grpGeneticAlgorithm, SWT.NONE);
+		btnGeneticAlgorithm.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				//TODO once pulled GA, then make it happen at the push of this button
+			}
+		});
+		btnGeneticAlgorithm.setBounds(345, 235, 75, 25);
+		btnGeneticAlgorithm.setText("Algorithize!");
+		//end genetic algorithm group
+		
+	}
+	
+	private void createConsoleOutputDA(Group grpDataAnalyzer) {
+		Group grpConsoleOutput_DA = new Group(grpDataAnalyzer, SWT.NONE);
+		grpConsoleOutput_DA.setText("Console Output");
+		grpConsoleOutput_DA.setBounds(452, 10, 430, 549);
+		
+		styledTextConsoleOutput = new StyledText(grpConsoleOutput_DA, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
+		styledTextConsoleOutput.setBounds(10, 20, 410, 519);
+		//end console output group
+		
+	}
+
 }//end class
